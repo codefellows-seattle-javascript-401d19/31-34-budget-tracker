51d957af162426a189c3a0d8fe72cc658c6016e9
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-16 14:02:10 -0800

    initial render and webpack working properly

---
+++ lab-cameron/src/component/app/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default App;
---
+++ lab-cameron/src/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import App from './component/app';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, container);
