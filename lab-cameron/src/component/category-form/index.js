927ce4ac312b38e901f594deaf5b1286ad4a31df
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-16 14:55:38 -0800

    category-form implemented

--- lab-cameron/src/component/category-form/index.js
+++ lab-cameron/src/component/category-form/index.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import uuidv1 from 'uuid/v1';
+
+const emptyState = {
+  id: uuidv1(),
+  timestamp: new Date(),
+  name: '',
+  budget: 0,
+};
 
 class CategoryForm extends Component {
   constructor(props) {
     super(props);
 
+    this.state = this.props.category || emptyState;
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onComplete(this.state);
+    this.setState(emptyState);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.category) {
+      this.setState(nextProps.category);
+    }
   }
 
   render() {
+    const buttonText = this.props.category ? 'update category' : 'create category';
     return (
-      <div>what</div>
+      <form className='category-form' onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          name='name'
+          placeholder='name'
+          onChange={this.handleChange}
+        />
+        <input
+          type='text'
+          name='budget'
+          placeholder='budget'
+          onChange={this.handleChange}
+        />
+        <button type='submit'>{ buttonText }</button>
+      </form>
     );
   }
 }
