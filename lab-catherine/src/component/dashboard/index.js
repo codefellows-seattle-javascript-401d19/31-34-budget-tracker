b98319e3a2f6bb23ece8798c6af09dc1d8acb337
Catherine Looper <loopercat@gmail.com>
2018-01-16 14:45:44 -0800

    Built out dashboard index.js

---
+++ lab-catherine/src/component/dashboard/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import CategoryForm from '../category-form';
+import * as category from '../../action/category';
+
+class Dashboard extends React.Component {
+  render() {
+    let {
+      categories, 
+      categoryCreate,
+      categoryUpdate,
+      categoryRemove,
+    } = this.props;
+
+    return (
+      <div className='dashboard'>
+        <CategoryForm onComplete={categoryCreate} />
+        {
+          categories.map((category, index) => 
+            <div key={index}>
+              <h2>{category.name}</h2>
+              <button onClick={() => categoryRemove(category)}>delete</button>
+              <CategoryForm category={category} onComplete={categoryUpdate} />
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+let mapStateToProps = (state) => {
+  return {
+    categories: state,
+  };
+};
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    categoryCreate: (data) => dispatch(category.createAction(data)),
+    categoryUpdate: (data) => dispatch(category.updateAction(data)),
+    categoryRemove: (data) => dispatch(category.removeAction(data)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
