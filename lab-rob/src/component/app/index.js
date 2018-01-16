01e4bf9be01c11b27c7847c5cd644c2266bd6973
Robert Reed <robert.mc.reed@gmail.com>
2018-01-16 15:00:25 -0800

    app.js

---
+++ lab-rob/src/component/app/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+import Landing from '../landing';
+import Dashboard from '../dashboard';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <header>
+              <h1>Budget Calculator - <em>React!</em></h1>
+              <nav>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/dashboard'>Calculator</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
