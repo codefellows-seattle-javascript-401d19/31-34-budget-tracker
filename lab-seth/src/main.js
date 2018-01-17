dc60b49860a1ea801099280f66f232ba6a5ba991
SethDonohue <donohue.seth@gmail.com>
2018-01-17 15:11:14 -0800

    beinning the shift over to stuff and things big time

--- lab-seth/src/main.js
+++ lab-seth/src/main.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
 import App from './component/app';
-import categoriesReducer from './reducer/categories.js';
+import reducer from './reducer';
 
-let store = createStore(categoriesReducer);
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+// let store = createStore(reducer);
+
+let middleware = {};
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 store.subscribe(() => {
   console.log('__STATE__', store.getState());
