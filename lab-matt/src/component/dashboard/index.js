d401b219aa1c645f5ad509c8f0d423e86bf30222
snobeard <fatmattmatt@gmail.com>
2018-01-16 19:08:00 -0800

    refactored to have expense-item component

--- lab-matt/src/component/dashboard/index.js
+++ lab-matt/src/component/dashboard/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as expense from '../../action/expense';
 import ExpenseForm from '../expense-form';
+import ExpenseItem from '../expense-item';
 
 class Dashboard extends React.Component {
   render() {
-    let { expenses, expenseCreate, expenseUpdate, expenseRemove } = this.props;
+    let { expenses, expenseCreate } = this.props;
     return (
       <div className='dashboard'>
         <ExpenseForm onComplete={expenseCreate} />
         {
           expenses.map((expense, index) => {
-            let title = expense.title !== '' ? expense.title : `'no title'`;
             return <div key={index}>
-              <h2> {title} | ${expense.price}</h2>
-              <button onClick={() => expenseRemove(expense)}> delete </button>
-              <ExpenseForm expense={expense} onComplete={expenseUpdate} />
+              <ExpenseItem expense={expense} />
             </div>;
           })
         }
@@ -33,8 +31,6 @@ let mapStateToProps = (state) => {
 let mapDispatchToProps = (dispatch) => {
   return {
     expenseCreate: (data) => dispatch(expense.createAction(data)),
-    expenseUpdate: (data) => dispatch(expense.updateAction(data)),
-    expenseRemove: (data) => dispatch(expense.removeAction(data)),
   };
 };
 
