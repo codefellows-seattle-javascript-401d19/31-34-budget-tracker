2dc34b30f2fcf60fc04bc1ef6543e194321ccf8e
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-18 15:41:54 -0800

    refactor expense item so that form to update only shows when item is double clicked

--- lab-shannon/src/component/expense-item/index.js
+++ lab-shannon/src/component/expense-item/index.js
@@ -3,9 +3,18 @@ import ExpenseForm from '../expense-form';
 import * as expenseActions from '../../action/expense';
 import { connect } from 'react-redux';
 
-let emptyState = {};
-
 class ExpenseItem extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {editing: false};
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleUpdate(expense){
+    this.props.expenseUpdate(expense);
+    this.setState({editing: false});
+  }
+
   render(){
     let {
       expenseDestroy,
@@ -14,12 +23,17 @@ class ExpenseItem extends React.Component{
 
     return(
       this.props.expenses.map((expense, index) => {
+        let editingJSX = <ExpenseForm expense={expense} onComplete={this.handleUpdate}/>;
+        let nonEditingJSX = <h4><em>{expense.name}:</em> ${expense.price}</h4>;
+        let shownJSX = this.state.editing ? editingJSX : nonEditingJSX;
+
         return(
-          <div key={index}>
-            <h4><em>{expense.name}:</em> ${expense.price}</h4>
-            <ExpenseForm expense={expense} onComplete={expenseUpdate}/>
-            <button onClick={() => expenseDestroy(expense)}>Delete Expense</button>
-          </div>
+          <main key={index} onDoubleClick={() => this.setState({editing: true})}>
+            <div>
+              {shownJSX}
+              <button onClick={() => expenseDestroy(expense)}>Delete Expense</button>
+            </div>
+          </main>
         );
       })
     );
