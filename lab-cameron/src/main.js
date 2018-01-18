c06ebbea70a86aff31e30051cfe454bae88b9758
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-18 14:48:38 -0800

    reporter and session middleware setup

--- lab-cameron/src/main.js
+++ lab-cameron/src/main.js
@@ -2,16 +2,21 @@ import './style/main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
 import App from './component/app';
-
 import reducer from './reducer';
+
+import session from './lib/redux-session';
+import reporter from './lib/redux-reporter';
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware()
+  )
 );
 
 const container = document.createElement('div');
