54e68a637d5fba8daede6d013eca9261f68db8e4
Catherine Looper <loopercat@gmail.com>
2018-01-18 20:03:46 -0800

    Scaffolded componentWillMount for retrieving local storage

--- lab-catherine/src/component/app/index.js
+++ lab-catherine/src/component/app/index.js
@@ -4,6 +4,25 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Dashboard from '../dashboard';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount(categories, expenses) {
+    try {
+      const cachedCategories = localStorage.getItem('categories');
+      if(cachedCategories) {
+        this.setState({ categories: JSON.parse(cachedCategories)});
+      }
+
+      const cachedExpenses = localStorage.getItem('expenses');
+      if(cachedExpenses) {
+        this.setState({expenses: JSON.parse(cachedExpenses)});
+      }
+    }
+    catch(error) {} //eslint-disable-line
+  }
+
   render() {
     return (
       <div className='app'>
