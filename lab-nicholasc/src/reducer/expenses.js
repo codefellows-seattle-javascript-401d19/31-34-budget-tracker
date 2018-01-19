db244b37f80639d06737ea3736372601b459f171
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-01-18 17:19:32 -0800

    actions ready

--- lab-nicholasc/src/reducer/expenses.js
+++ lab-nicholasc/src/reducer/expenses.js
@@ -1,14 +1,40 @@
-const emptyState = [];
+const emptyState = {};
 
 export default (state = emptyState, {type, payload}) => {
+  let categoryID, categoryExpenses, updatedExpenses, newState;
+
   switch(type){
     case 'CATEGORY_CREATE':
-      return [...state, payload];
-    case 'CATEGORY_UPDATE':
-      return state.map(category => category.id === payload.id ? payload : category);
+      return {...state, [payload.id] : []};
     case 'CATEGORY_REMOVE':
-      return state.filter(category => category.id !== payload.id);
-    case 'CATEGORY_CLEAR':
+      newState = {...state};
+      delete newState[payload.id];
+      return newState;
+
+    case 'EXPENSE_CREATE':
+      categoryID = payload.categoryID;
+      categoryExpenses = state[categoryID];
+      updatedExpenses = [...categoryExpenses, payload];
+      return {
+        ...state, [payload.categoryID] : updatedExpenses,
+      };
+    case 'EXPENSE_UPDATE':
+      categoryID = payload.categoryID;
+      categoryExpenses = state[categoryID];
+      updatedExpenses = categoryExpenses.map(
+        expense => expense.id === payload.id ? payload : expense);
+      return {
+        ...state, [payload.categoryID] : updatedExpenses,
+      };
+    case 'EXPENSE_REMOVE':
+      categoryID = payload.categoryID;
+      categoryExpenses = state[categoryID];
+      updatedExpenses = categoryExpenses.filter(
+        expense => expense.id !== payload.id);
+      return {
+        ...state, [payload.categoryID] : updatedExpenses,
+      };
+    case 'EXPENSE_CLEAR':
       return emptyState;
     default:
       return state;
