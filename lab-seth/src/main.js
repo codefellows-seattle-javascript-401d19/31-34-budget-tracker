6d851b24738fc593684b84f479dcc4e5c957f793
SethDonohue <donohue.seth@gmail.com>
2018-01-18 16:32:40 -0800

    middleware for redux session and reporter added

--- lab-seth/src/main.js
+++ lab-seth/src/main.js
@@ -2,25 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 import App from './component/app';
 import reducer from './reducer';
 
-import {composeWithDevTools} from 'redux-devtools-extension';
+import session from './lib/redux-session';
+import reporter from './lib/redux-reporter';
 
-let middleware = {};
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
-
-store.subscribe(() => {
-  // console.log('__STATE__', store.getState());
-});
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(reporter, session)
+));
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 
 ReactDom.render(
-  //allows us to connect to the the store at any level we want.
   <Provider store={store}> 
     <App />
   </Provider>, container);
